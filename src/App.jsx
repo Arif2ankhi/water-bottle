c2b4f284f133a7e1f36a69956268cd6536dfa71c
Arif2ankhi <arif2ankhi@gmail.com>
2024-10-22 01:52:22 +0100

    Water bottle project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Header from './Components/Header'
+import Bottles from './Components/Bottles/Bottles'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <Header></Header>  
+      <Bottles></Bottles>    
+    </>
+  )
+}
+
+export default App
